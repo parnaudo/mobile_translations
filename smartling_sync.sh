0668ca847db8560c1f7e2e40aead4b350f23441e
parnaudo <parnaudo@gmail.com>
2013-12-10 10:39:47 -0800

    Update smartling_sync.sh
    fixing issues with the git pull

--- smartling_sync.sh
+++ smartling_sync.sh
@@ -2,17 +2,16 @@
 printf "\nrunning on $(date -u)"
 cd ios
 printf "\nSyncing ios"
-git pull
+git pull origin master
 printf "\ncurling ios"
-curl -F "file=@/root/translations/ios/SmugChat/Base.lproj/Localizable.strings;type=text/plain" "https://api.smartling.com/v1/file/plupload?apiKey=fdc6e190-e855-4588-bfe0-74b7006aeab1&projectId=59d739850&fileUri=%2Ffiles%2FLocalizable_final.strings&fileType=IOS"
+curl -F "file=@/root/translations/ios/SmugChat/Base.lproj/Localizable.strings;type=text/plain" "https://api.smartling.com/v1/file/plupload?apiKey=fdc6e190-e855-4588-bfe0-74b7006aeab1&projectId=59d739850&fileUri=%2Ffiles%2FLocalizable_final.strings&fileType=IOS&approved=1"
 cd ../android
 printf "\nSyncing Android"
-git pull
+git pull origin master
 printf "\ncurling android"
-curl -F "file=@/root/translations/android/res/values/strings.xml;type=text/xml" "https://api.smartling.com/v1/file/plupload?apiKey=fdc6e190-e855-4588-bfe0-74b7006aeab1&projectId=59d739850&fileUri=/files/android.xml&fileType=android"
+curl -F "file=@/root/translations/android/res/values/strings.xml;type=text/xml" "https://api.smartling.com/v1/file/plupload?apiKey=fdc6e190-e855-4588-bfe0-74b7006aeab1&projectId=59d739850&fileUri=/files/android.xml&fileType=android&approved=1"
 cd ../ruby	
 printf "\nSyncing Ruby"
-git pull
+git pull origin master
 printf "\nCurling Ruby"
-curl -F "file=@/root/translations/ruby/config/locales/en.yml;type=text/plain" "https://api.smartling.com/v1/file/plupload?apiKey=fdc6e190-e855-4588-bfe0-74b7006aeab1&projectId=59d739850&fileUri=/files/en.yml&fileType=YAML"
-
+curl -F "file=@/root/translations/ruby/config/locales/en.yml;type=text/plain" "https://api.smartling.com/v1/file/plupload?apiKey=fdc6e190-e855-4588-bfe0-74b7006aeab1&projectId=59d739850&fileUri=/files/en.yml&fileType=YAML&approved=1"
