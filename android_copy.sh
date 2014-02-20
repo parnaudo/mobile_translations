b5fe5d2eb1a441b28a2aa17c785d0b8fff922644
parnaudo <paul@appredeem.com>
2014-02-20 12:22:41 -0800

    moving android

---
+++ android_copy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cp translations/android/es-android.xml ../Android/SmugChat_Android/res/values-es/strings.xml 
+cp translations/android/da-DK-android.xml ../Android/SmugChat_Android/res/values-da/strings.xml
+cp translations/android/de-DE-android.xml  ../Android/SmugChat_Android/res/values-de/strings.xml
+cp translations/android/fi-FI-android.xml ../Android/SmugChat_Android/res/values-fi/strings.xml
+cp translations/android/fr-FR-android.xml ../Android/SmugChat_Android/res/values-fr/strings.xml
+cp translations/android/id-ID-android.xml ../Android/SmugChat_Android/res/values-in/strings.xml
+cp translations/android/it-IT-android.xml ../Android/SmugChat_Android/res/values-it/strings.xml
+cp translations/android/ja-JP-android.xml ../Android/SmugChat_Android/res//values-ja/strings.xml
+cp translations/android/ko-KR-android.xml ../Android/SmugChat_Android/res/values-ko/strings.xml
+cp translations/android/nb-NO-android.xml ../Android/SmugChat_Android/res/values-nb/strings.xml
+cp translations/android/nl-NL-android.xml ../Android/SmugChat_Android/res/values-nl/strings.xml
+cp translations/android/pt-BR-android.xml ../Android/SmugChat_Android/res/values-pt/strings.xml
+cp translations/android/ru-RU-android.xml ../Android/SmugChat_Android/res/values-ru/strings.xml
+cp translations/android/sv-SE-android.xml ../Android/SmugChat_Android/res/values-sv/strings.xml
+cp translations/android/th-TH-android.xml ../Android/SmugChat_Android/res/values-th/strings.xml
+cp translations/android/zh-TW-android.xml ../Android/SmugChat_Android/res/values-zh-rTW/strings.xml
+cp translations/android/zh-CN-android.xml ../Android/SmugChat_Android/res/values-zh-rCN/strings.xml
